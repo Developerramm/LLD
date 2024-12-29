ab92a7b7bb8b6442903178a8114403d9fc5d356e
ram kumar <rr697144@gmail.com>
2024-12-29 13:13:49 +0530

    cpp basic setup

---
+++ basiccpp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "My name is ram kumar " << endl;
+    int age;
+    cout << "Enter your age ";
+    cin >> age;
+    cout << "your age is " << age << endl;
+
+    return 0;
+}
