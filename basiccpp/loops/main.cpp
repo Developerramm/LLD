b2cad09b736ac52c84f54532847da5428bf7339b
ram kumar <rr697144@gmail.com>
2025-01-01 16:06:58 +0530

    for loop in cpp

---
+++ basiccpp/loops/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "ram kumar" << endl;
+    cout << "ram kumar" << endl;
+    cout << "ram kumar" << endl;
+
+    for(int count = 1; count <= 10; count++){
+        cout << "ram kumar" << endl;
+    }
+
+    for (int  i = 0; i < 10; i++)
+    {
+        cout << i + 1 << endl;
+    }
+    
+
+    return 0;
+}
