37d4f9d6cf0b84f72aaf4c528732a7acf4b0ccd1
ram kumar <rr697144@gmail.com>
2025-01-02 09:01:42 +0530

    unary operator

---
+++ basiccpp/operator/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Operator in cpp " << endl;
+    int a = 5;
+    cout << ++a << endl;
+
+    cout << a++ << endl;
+    cout << a << endl;
+
+    int b = 10;
+    cout << b-- << endl;
+    cout << b << endl;
+    cout << --b << endl;
+    cout << b << endl;
+
+    return 0;
+}
