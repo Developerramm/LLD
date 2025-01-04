5de6cb4bdf2f7a8e7365db815d20c9766da59055
ram kumar <rr697144@gmail.com>
2025-01-04 14:40:50 +0530

    type conversion

---
+++ basiccpp/typecast/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int num1 = 10;
+    // float num2 = 5.5;
+    // float result = num1 + num2;
+    // cout << result << endl;
+    // cout << type_info(result) << endl;
+
+    // char to int 
+    // char ch = 'A';
+    // int a = ch + 1;
+    // cout << a << endl;
+
+
+    // integer to char conversion
+    // int a = 97;
+    // char ch = a;
+    // cout << ch << endl;
+
+
+    // char ch = 'A';
+    // cout << ch + 1 << endl;
+    // cout << ch << endl;
+
+    // cout << ch + int(1) << endl;
+
+
+    return 0;
+}
