178e2c3d7b3ec3667156684554635bd48b444940
ram kumar <rr697144@gmail.com>
2025-01-05 08:59:38 +0530

    char array basic

---
+++ basiccpp/strings/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Char array and string " << endl;
+
+    // char arr[10];
+    // cout << arr[3] << endl;
+
+    // char arr[10] = "ramkumar";
+    // cout << arr << endl;
+    // cout << arr[0] << endl;
+    // cout << arr[5] << endl;
+    // cout << arr[8] << endl;
+
+    // int asciValue = arr[8];
+    // cout << asciValue << endl;
+
+    // char arr[4];
+
+    // arr[0] = 'r';
+    // arr[1] = 'a';
+    // arr[2] = 'm';
+    // arr[3] = 'k';
+    // arr[4] = '\0';
+
+    // int arr[4] = {1,2,3,4};
+    // cout << arr << endl;
+
+
+    // char brr[10] = "ramku";
+    // cout << brr << endl;
+
+    // for(int i = 0; i<5; i++){
+    //     cout << brr[i] << " ";
+    // }
+
+
+
+
+    return 0;
+}
