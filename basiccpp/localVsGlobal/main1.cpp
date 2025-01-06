39f97bda51adb94194663b971d5487775e6a02f0
ram kumar <rr697144@gmail.com>
2025-01-06 16:34:46 +0530

    const keyword in cpp

---
+++ basiccpp/localVsGlobal/main1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // const int x = 5; // constant varialbe
+    // cout << x << endl;
+    // x = 10;
+
+    // int *p = &x;
+    // *p = 10;
+
+    
+
+    const int *a = new int;
+    *a = 2;
+    cout << *a << endl;
+    delete a;
+    int b = 5;
+    a = &b;
+    cout << *a << endl;
+
+
+    return 0;
+}
