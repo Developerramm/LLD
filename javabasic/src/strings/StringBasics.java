92650d9a82a83c4164d83971c681373f41efa3e6
ram kumar <rr697144@gmail.com>
2025-01-12 21:29:48 +0530

    string create in java

---
+++ javabasic/src/strings/StringBasics.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class StringBasics {
+    public static void main(String[] args) {
+        String name = "Ram Kumar";
+        System.out.println(name);
+
+        String firstName = new String("Ram");
+        System.out.println(firstName);
+
+        System.out.println(firstName + name);
+
+        firstName = "O";
+        System.out.println(firstName);
+
+    }
+}
