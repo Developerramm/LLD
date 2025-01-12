abae9b343e12f420b139f6c1fc9cc7dd28c3df16
ram kumar <rr697144@gmail.com>
2025-01-12 21:35:36 +0530

    string method in java

--- javabasic/src/strings/StringBasics.java
+++ javabasic/src/strings/StringBasics.java
@@ -13,5 +13,22 @@ public class StringBasics {
         firstName = "O";
         System.out.println(firstName);
 
+        String str1 = "ram";
+        String str2 = "ram";
+        String str3 = new String("ram");
+
+
+        if(str1.equals(str3)){
+            System.out.println("String are equal");
+        }else {
+            System.out.println("String are not equal");
+        }
+
+        if(str1 == str3){
+            System.out.println("String are equal");
+        }else {
+            System.out.println("String are not equal");
+        }
+
     }
 }
