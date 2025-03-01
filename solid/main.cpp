a3e4098a638af29410c1ed01f792df30c8284a1d
ram kumar <rr697144@gmail.com>
2025-03-01 10:08:56 +0530

    srp principles

---
+++ solid/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class userInfo
+{
+public:
+    void addUser()
+    {
+    }
+    void deleteUser()
+    {
+    }
+};
+
+class logger
+{
+
+public:
+    void logError()
+    {
+    }
+};
